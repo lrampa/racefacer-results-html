de35885b6e3e38d21702eb533fe5523edc59bb82
Lukas Rampa <lukas.rampa@aimtecglobal.com>
2024-10-12 00:46:50 +0200

    Index content initialization

--- server.py
+++ server.py
@@ -6,8 +6,15 @@ import logging
 import time
 from datetime import datetime
 
+
+ws_url = 'https://live.racefacer.com:3123/socket.io/'
+# ws_url = 'http://localhost:8080'
+# ws_channel = 'kartarenacheb'
+ws_channel = 'denizkarting'
+
+
 logging.basicConfig(
-    # filename="socketio.log",
+    filename="socketio.log",
     # format="%(asctime)s %(levelname)s %(name)s %(message)s",
     format="%(asctime)s %(levelname)s %(message)s",
     level=logging.DEBUG,
@@ -24,11 +31,6 @@ sio = socketio.Client()
 latest_message = None
 
 def start_socketio_client():
-    ws_url = 'https://live.racefacer.com:3123/socket.io/'
-    # ws_url = 'http://localhost:8080'
-    # ws_channel = 'kartarenacheb'
-    ws_channel = 'denizkarting'
-    min_lap_length_to_report = 30 # seconds
 
     @sio.event
     def connect():
@@ -47,43 +49,7 @@ def start_socketio_client():
     def message(data):
         logging.info(f"message received: {ws_channel} - {data}")
 
-        results = []
-        if 'runs' in data['data']:
-            # iterate over data['data']['runs'] and create a tuple for each run
-            for run in data['data']['runs']:
-                results.append({
-                    'kart': run.get('kart', ''), # "kart": "18",
-                    'total_laps': run.get('total_laps', ''), # "total_laps": 8,
-                    'last_time': run.get('last_time', 0), # "last_time": "1:10.574",
-                    'last_time_raw': run.get('last_time_raw', 0), # "last_time_raw": 70574,
-                    'last_passing': run.get('last_passing', 0), # "last_passing": 1721900723855,
-                    'current_lap_start_timestamp': run.get('current_lap_start_timestamp', -1), # "current_lap_start_timestamp": 1721900755,
-                    'current_lap_start_microtimestamp': run.get('current_lap_start_microtimestamp', -1) # "current_lap_start_microtimestamp": 1721900726428,
-                })
-
-        # get current timestamp in seconds
-        current_timestamp = time.time()
-        current_timestamp_formatted = datetime.fromtimestamp(current_timestamp).strftime('%H:%M:%S')
-
-        # Find results where the current_lap_start_timestamp is more than 60 seconds old
-        filtered_results = []
-        for result in results:
-            current_lap_start_timestamp_formatted = datetime.fromtimestamp(result['current_lap_start_timestamp']).strftime('%H:%M:%S')
-
-            diff = int(current_timestamp - result['current_lap_start_timestamp'])
-            minutes, seconds = divmod(diff, 60)
-            diff_formatted = f"{minutes:02}:{seconds:02}"
-
-            logging.info(f"Adding result:   kart {result['kart']}, {current_timestamp_formatted} - {current_lap_start_timestamp_formatted} = {diff_formatted} > {min_lap_length_to_report}")
-            result['diff_formatted'] = diff_formatted
-            filtered_results.append(result)
-
-        # Sort the filtered results by the current_lap_start_timestamp in ascending order
-        sorted_results = sorted(filtered_results, key=lambda x: x['current_lap_start_timestamp'])
-
-        # Print the sorted results
-        for result in sorted_results:
-            logging.info(f"kart: {result['kart']}, total_laps: {result['total_laps']}, last_time: {result['last_time']}, last_time_raw: {result['last_time_raw']}, last_passing: {result['last_passing']}, current_lap_start_timestamp: {result['current_lap_start_timestamp']}, current_lap_start_microtimestamp: {result['current_lap_start_microtimestamp']}")
+        sorted_results = process_data(data)
 
         global latest_message
         latest_message = sorted_results
@@ -122,16 +88,71 @@ def index():
     # Find out, if thread named "ws_client" exists
     threads = threading.enumerate()
     if not any(thread.name == "ws_client" for thread in threads):
-        logging.info('before thread start')
+        logging.debug('before thread start')
         thread = threading.Thread(name="ws_client", target=start_socketio_client)
         thread.daemon = True
         thread.start()
-        logging.info('after thread start')
+        logging.debug('after thread start')
 
+    data = fetch_data()
+    results = process_data(data)
     global latest_message
-    latest_message = None  # Reset the message when the page is loaded
+    latest_message = results  # Reset the message when the page is loaded
 
     return render_template('index.html', message=latest_message)
+
+def fetch_data():
+    # Fetch json response from https://live.racefacer.com/ajax/live-data?slug=denizkarting
+    import requests
+    import json
+    url = f"https://live.racefacer.com/ajax/live-data?slug={ws_channel}"
+    response = requests.get(url)
+    data = json.loads(response.text)
+    return data
+
+
+def process_data(data):
+    results = []
+
+    if 'runs' in data['data']:
+        # iterate over data['data']['runs'] and create a tuple for each run
+        for run in data['data']['runs']:
+            results.append({
+                'kart': run.get('kart', ''), # "kart": "18",
+                'total_laps': run.get('total_laps', ''), # "total_laps": 8,
+                'last_time': run.get('last_time', 0), # "last_time": "1:10.574",
+                'last_time_raw': run.get('last_time_raw', 0), # "last_time_raw": 70574,
+                'last_passing': run.get('last_passing', 0), # "last_passing": 1721900723855,
+                'current_lap_start_timestamp': run.get('current_lap_start_timestamp', -1), # "current_lap_start_timestamp": 1721900755,
+                'current_lap_start_microtimestamp': run.get('current_lap_start_microtimestamp', -1) # "current_lap_start_microtimestamp": 1721900726428,
+            })
+
+    # get current timestamp in seconds
+    current_timestamp = time.time()
+    current_timestamp_formatted = datetime.fromtimestamp(current_timestamp).strftime('%H:%M:%S')
+
+    # Find results where the current_lap_start_timestamp is more than 60 seconds old
+    filtered_results = []
+    for result in results:
+        current_lap_start_timestamp_formatted = datetime.fromtimestamp(result['current_lap_start_timestamp']).strftime('%H:%M:%S')
+
+        diff = int(current_timestamp - result['current_lap_start_timestamp'])
+        minutes, seconds = divmod(diff, 60)
+        diff_formatted = f"{minutes:02}:{seconds:02}"
+
+        logging.info(f"Adding result:   kart {result['kart']}, {current_timestamp_formatted} - {current_lap_start_timestamp_formatted} = {diff_formatted}")
+        result['diff_formatted'] = diff_formatted
+        filtered_results.append(result)
+
+    # Sort the filtered results by the current_lap_start_timestamp in ascending order
+    sorted_results = sorted(filtered_results, key=lambda x: x['current_lap_start_timestamp'])
+
+    # Print the sorted results
+    for result in sorted_results:
+        logging.info(f"kart: {result['kart']}, total_laps: {result['total_laps']}, last_time: {result['last_time']}, last_time_raw: {result['last_time_raw']}, last_passing: {result['last_passing']}, current_lap_start_timestamp: {result['current_lap_start_timestamp']}, current_lap_start_microtimestamp: {result['current_lap_start_microtimestamp']}")
+    
+    return sorted_results
+
 
 if __name__ == '__main__':
     app.run()
